e900a6df48073440939ae7e8b1334b6b365656e6
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-12-30 00:35:28 -0500

    Add new controller for fetching API data & attempt (unsuccessfully) to use Learning Studio gems.

---
+++ app/helpers/data_fetch_helper.rb
@@ -0,0 +1,2 @@
+module DataFetchHelper
+end
---
+++ test/controllers/data_fetch_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DataFetchControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
