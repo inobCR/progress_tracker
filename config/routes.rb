8f9f54ca253c5368782c97e4b30fef881affcc26
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-01-01 18:52:24 -0500

    Add data fetch page for Terms API.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'announcements',  to: 'data_fetch#announcements'
   get 'schedules',      to: 'data_fetch#schedules'
   get 'course_info',    to: 'data_fetch#course_info'
+  get 'terms',          to: 'data_fetch#terms'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
