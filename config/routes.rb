463bed07fba0cb7f5f8c9178c9c8202fc443eb9e
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-01-01 15:27:32 -0500

    Add data fetch page for schedules.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'grades',         to: 'data_fetch#grades'
   get 'content',        to: 'data_fetch#content'
   get 'announcements',  to: 'data_fetch#announcements'
+  get 'schedules',      to: 'data_fetch#schedules'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
